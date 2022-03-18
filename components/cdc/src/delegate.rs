ad344d23ab9fad15dfc5e6c1b96ccedc8a2078c0
zeminzhou <zeminzhou_@outlook.com>
2022-03-18 14:01:05 +0800

    decode rawkv key and value

--- components/cdc/src/delegate.rs
+++ components/cdc/src/delegate.rs
@@ -733,7 +733,7 @@ impl Delegate {
             "" | "default" => {
                 let mut row = EventRow::default();
                 let (key, ts) =
-                    APIV2::decode_raw_key_owned(Key::from_raw(put.get_key()), true).unwrap();
+                    APIV2::decode_raw_key_owned(Key::from_encoded(put.take_key()), true).unwrap();
                 row.commit_ts = ts.unwrap().into_inner(); // would be no causal timestamp on data written by old version.
 
                 let raw_value = APIV2::decode_raw_value_owned(put.get_value().to_vec()).unwrap();
